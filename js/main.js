f94fceb46671b724d64151f9da6b881ebfe19acc
Bruce Skyz <144792190+Skyz254@users.noreply.github.com>
2025-04-11 11:25:57 +0000

    11th April

--- js/main.js
+++ js/main.js
@@ -47,3 +47,27 @@ function showSlider(type){
         next.click();
     }, timeAutoNext)
 }
+
+//navbar
+ const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((LargestContentfulPaint, index) => {
+            if (navLinks.style.animation) {
+                navLinks.style.animation = '';
+            } else {
+                navLinks.style.animation = 'navLinkFade 0.5s ease forwards'
+            }
+        })
+
+    })
+
+ }
